222f497c24eed32b5e44da834b67afa98fd1a222
Craig Cook <boycook@me.com>
2014-04-19 23:15:58 +0100

    Started data load scrips for model

--- data/load_data.sh
+++ data/load_data.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 BASE_URL=http://localhost:9000
-TYPE_URL=$BASE_URL/types
+MODEL_URL=$BASE_URL/_model
 
-curl -X POST -H "Content-Type: application/json" --data @types/HRTS1.json $TYPE_URL
-curl -X POST -H "Content-Type: application/json" --data @ypes/HRMT1.json $TYPE_URL
-curl -X POST -H "Content-Type: application/json" --data @ypes/HRMT2.json $TYPE_URL
+curl -X POST -H "Content-Type: application/json" --data @types/domain.json $MODEL_URL
+curl -X POST -H "Content-Type: application/json" --data @ypes/subdomain.json $MODEL_URL
